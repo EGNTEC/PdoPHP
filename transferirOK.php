b7a7af5ae2528ff231cdb94e84c9ea22bf910202
egntec <emerson.gonas@gmail.com>
2018-06-24 17:41:46 -0300

    Inserindo o Update bugado.

--- transferirOK.php
+++ transferirOK.php
@@ -5,21 +5,34 @@ if(isset($_POST['idr'])){
 
     $conn = new PDO('mysql:host=localhost;dbname=pdo','root','root');
 
-    $sql = "Select id From contas Where id=".$_POST['idd'];
-    $query =  $conn->query($sql)->fetchAll();
+    $sql = "SELECT Count(*) From contas Where id=".$_POST['idd'];
+    $query =  $conn->query($sql);
 
     if(count($query) > 0){
-        $sql = "Select id From contas Where id=".$_POST['idr'];
-        $query =  $conn->query($sql)->fetchAll();
+        $sql = "SELECT Count(*) From contas Where id=".$_POST['idr'];
+        $query =  $conn->query($sql);
 
 
-        if(count($query) > 0){
-            $sql = "Select saldo From contas Where id=".$_POST['idr'];
+        if($query->fetchColumn() > 0){
+            $sql = "SELECT saldo From contas Where id=".$_POST['idr'];
             
             foreach ($conn->query($sql) As $row) {
                 if($row['saldo'] >= $_POST["valor"]){
+                    //retirar
+                    $updateRemetenteSQL = "UPDATE contas Set saldo = saldo - ".$_POST["valor"]." Where id=".$_POST['idr'];
+                    $updateRemetenteQuery = $conn->query($updateSQL);
+                    
+                    if($updateRemetenteQuery->rowCount() > 0){
 
-                    echo"Transferencia OK!";
+                        //Adicionar
+                        $updateDestinatarioSQL = "UPDATE contas Set saldo = saldo + ".$_POST["valor"]." Where id=".$_POST['idd'];
+                        $updateDestinatarioQuery = $conn->query($updateSQL);
+
+                        if($updateDestinatarioQuery->rowCount() > 0){
+                            echo"Transferencia OK!";
+                        }
+
+                    }                    
                 }else{
                     echo"Transferencia NAO OK!";
                 }
